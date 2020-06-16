2863146d828656bfeb008e9a4229bfe0996ed5f3
SeacenLiu <1016667972@qq.com>
2020-06-16 08:08:59 +0800

    原始 demo 划分

---
+++ example/lib/live_test/live_anchor_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LiveAnchorPage extends StatefulWidget {
+  @override
+  _LiveAnchorPageState createState() => _LiveAnchorPageState();
+}
+
+class _LiveAnchorPageState extends State<LiveAnchorPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
